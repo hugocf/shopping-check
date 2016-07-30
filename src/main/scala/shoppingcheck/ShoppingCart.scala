a5bf310edb7ad3223e36d80fd2631192d2fb8066
Hugo Ferreira <hugo@ferreira.cc>
2016-07-30 14:10:52 +0000

    Add bananas and include them in the apples promo

--- src/main/scala/shoppingcheck/ShoppingCart.scala
+++ src/main/scala/shoppingcheck/ShoppingCart.scala
@@ -2,17 +2,23 @@ package shoppingcheck
 
 class ShoppingCart {
   type DiscountFunction = List[String] => Int
-  val prices = Map("apple" -> 60, "orange" -> 25)
+  private val prices = Map("apple" -> 60, "orange" -> 25, "banana" -> 20)
 
-  def simpleTotalCents(ps: List[String]): Int =
-    ps.map(price).sum
+  def simpleTotalCents(ps: List[String]): Int = ps.map(price).sum
 
   def promoTotalCents(ps: List[String], discounts: Seq[DiscountFunction]): Int =
     simpleTotalCents(ps: List[String]) - discounts.map(f => f(ps)).sum
 
-  def discountNforM(p: String, total: Int, pay: Int): DiscountFunction =
-    (ps) => ps.count(_ == p) / total * price(p) * (total - pay)
+  def discountNforM(d: String, total: Int, pay: Int): DiscountFunction =
+    discountNforM(Seq(d), total, pay)
 
-  private def price(p: String) = prices.getOrElse(p, 0)
+  def discountNforM(ds: Seq[String], total: Int, pay: Int): DiscountFunction =
+    (ps) => {
+      val discountItems = ps.collect{ case p if ds.contains(p) => p }
+      val countFree = discountItems.length / total * (total - pay)
+      discountItems.map(price).sorted.take(countFree).sum
+    }
+
+  def price(p: String) = prices.getOrElse(p, 0)
 
 }
--- src/test/scala/shoppingcheck/ShoppingCartSpec.scala
+++ src/test/scala/shoppingcheck/ShoppingCartSpec.scala
@@ -10,15 +10,15 @@ class ShoppingCartSpec extends BaseSpec {
 
   "simpleTotalCents" must {
     "add unit prices per item on the list, regardless of order" in {
-      forAll(apples, oranges) { (as, os) =>
-        val ps = shuffle(as ++ os)
-        sc.simpleTotalCents(ps) shouldBe costOfApples(as) + costOfOranges(os)
+      forAll(apples, oranges, bananas) { (as, os, bs) =>
+        val ps = shuffle(as ++ os ++ bs)
+        sc.simpleTotalCents(ps) shouldBe costOfApples(as) + costOfOranges(os) + costOfBananas(bs)
       }
     }
   }
 
   "promoTotalCents" must {
-    "apply discounts to the total value" in {
+    "apply the given discounts to the total value" in {
       val applePromo = sc.discountNforM(apple, 2, 1)
       val orangePromo = sc.discountNforM(orange, 3, 2)
       val promos = Seq(applePromo, orangePromo)
@@ -30,40 +30,85 @@ class ShoppingCartSpec extends BaseSpec {
     }
   }
 
-  "discountNforM" must {
-    "return half of the prices sum total as discount for even length apples" in {
-      val discountApples = (listOf(delay(apple)), "discounted")
-      forAll(discountApples) { as =>
-        sc.discountNforM(apple, 2, 1)(as ++ as) shouldBe costOfApples(as)
+  "discountNforM" when {
+
+    "discount considers only a single type of product" must {
+
+      "return half of the prices sum total as discount for even length apples" in {
+        val discountedApples = (listOf(delay(apple)), "discounted")
+        forAll(discountedApples) { as =>
+          sc.discountNforM(apple, 2, 1)(as ++ as) shouldBe costOfApples(as)
+        }
+      }
+
+      "not count the extra apple as discount for odd length apples" in {
+        val discountedApples = (listOf(delay(apple)), "discounted")
+        forAll(discountedApples) { as =>
+          sc.discountNforM(apple, 2, 1)(as ++ (as :+ apple)) shouldBe costOfApples(as)
+        }
+      }
+
+      "not apply discounts to lists without the discounted items" in {
+        forAll(oranges) { os =>
+          sc.discountNforM(apple, 2, 1)(os) shouldBe 0
+        }
+      }
+
+      "offer 1 orange whenever 3 are bought" in {
+        val discountedOranges = (listOf(delay(orange)), "discounted")
+        forAll(discountedOranges) { os =>
+          sc.discountNforM(orange, 3, 2)(os ++ os ++ os) shouldBe costOfOranges(os)
+        }
+      }
+
+      "ignore the extra orange(s) whenever odd numbers are bought" in {
+        val discountedOranges = (listOf(delay(orange)), "discounted")
+        forAll(discountedOranges) { os =>
+          val oranges = os ++ os ++ os
+          sc.discountNforM(orange, 3, 2)(oranges :+ orange) shouldBe costOfOranges(os)
+          sc.discountNforM(orange, 3, 2)(oranges :+ orange :+ orange) shouldBe costOfOranges(os)
+        }
       }
     }
 
-    "not count the extra apple as discount for odd length apples" in {
-      val discountApples = (listOf(delay(apple)), "discounted")
-      forAll(discountApples) { as =>
-        sc.discountNforM(apple, 2, 1)(as ++ (as :+ apple)) shouldBe costOfApples(as)
-      }
-    }
+    "discount considers a mix of different types of product" must {
 
-    "not apply discounts to lists without the discounted items" in {
-      forAll(oranges) { os =>
-        sc.discountNforM(apple, 2, 1)(os) shouldBe 0
-      }
-    }
+      "offer all the cheapest products first if they are both of the same number" in {
+        val sameLengthLists = for {
+          n <- posNum[Int]
+          as <- listOfN(n, delay(apple))
+          bs <- listOfN(n, delay(banana))
+        } yield (as, bs)
 
-    "offer 1 orange whenever 3 are bought" in {
-      val discountOranges = (listOf(delay(orange)), "discounted")
-      forAll(discountOranges) { os =>
-        sc.discountNforM(orange, 3, 2)(os ++ os ++ os) shouldBe costOfOranges(os)
+        forAll(sameLengthLists) { case (as, bs) =>
+          sc.discountNforM(Seq(apple, banana), 2, 1)(as ++ bs) shouldBe costOfBananas(bs)
+        }
       }
-    }
 
-    "ignore the extra orange(s) whenever odd numbers are bought" in {
-      val discountOranges = (listOf(delay(orange)), "discounted")
-      forAll(discountOranges) { os =>
-        val oranges = os ++ os ++ os
-        sc.discountNforM(orange, 3, 2)(oranges :+ orange) shouldBe costOfOranges(os)
-        sc.discountNforM(orange, 3, 2)(oranges :+ orange :+ orange) shouldBe costOfOranges(os)
+      "offer all the cheapest products and half of the remaining if the expensive list is bigger" in {
+        val lessCheaperProducts = for {
+          as <- apples
+          len <- choose(0, as.length)
+          bs <- listOfN(len, delay(banana))
+        } yield (as, bs)
+
+        forAll(lessCheaperProducts) { case (as, bs) =>
+          val expectedDiscount = costOfBananas(bs) + (as.length - bs.length) / 2 * 60
+          sc.discountNforM(Seq(apple, banana), 2, 1)(as ++ bs) shouldBe expectedDiscount
+        }
+      }
+
+      "offer all the cheapest products and half of the remaining if the cheapest list is bigger" in {
+        val lessCheaperProducts = for {
+          as <- apples
+          len <- posNum[Int] suchThat (_ > as.length)
+          bs <- listOfN(len, delay(banana))
+        } yield (as, bs)
+
+        forAll(lessCheaperProducts) { case (as, bs) =>
+          val expectedDiscount = (as.length * 20) + (bs.length - as.length) / 2 * 20
+          sc.discountNforM(Seq(apple, banana), 2, 1)(as ++ bs) shouldBe expectedDiscount
+        }
       }
     }
   }
@@ -72,10 +117,13 @@ class ShoppingCartSpec extends BaseSpec {
   val sc = new ShoppingCart
   val apple = "apple"
   val orange = "orange"
+  val banana = "banana"
 
   val apples = listOf(delay(apple))
   val oranges = listOf(delay(orange))
+  val bananas = listOf(delay(banana))
 
   def costOfApples(ps: List[String]) = ps.length * 60
   def costOfOranges(ps: List[String]) = ps.length * 25
+  def costOfBananas(ps: List[String]) = ps.length * 20
 }
