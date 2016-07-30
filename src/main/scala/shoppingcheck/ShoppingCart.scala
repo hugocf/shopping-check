02b8c2f5165cf0379ccf1d8e5e2f6b8bb041e7a5
Hugo Ferreira <hugo@ferreira.cc>
2016-07-30 01:41:44 +0100

    Apply discounts N for M

--- src/main/scala/shoppingcheck/ShoppingCart.scala
+++ src/main/scala/shoppingcheck/ShoppingCart.scala
@@ -3,6 +3,15 @@ package shoppingcheck
 class ShoppingCart {
   val prices = Map("apple" -> 60, "orange" -> 25)
 
-  def totalCents(ps: List[String]): Int = ps.map(prices.getOrElse(_, 0)).sum
+  private def price(p: String) = prices.getOrElse(p, 0)
+
+  def simpleTotalCents(ps: List[String]): Int =
+    ps.map(price).sum
+
+  def promoTotalCents(ps: List[String]): Int =
+    simpleTotalCents(ps: List[String]) - discountNforM(ps, "apple", 2, 1) - discountNforM(ps, "orange", 3, 2)
+
+  def discountNforM(ps: List[String], p: String, total: Int, pay: Int): Int =
+    ps.count(_ == p) / total * price(p) * (total - pay)
 
 }
--- src/test/scala/shoppingcheck/ShoppingCartSpec.scala
+++ src/test/scala/shoppingcheck/ShoppingCartSpec.scala
@@ -8,11 +8,60 @@ import scala.util.Random._
 class ShoppingCartSpec extends BaseSpec {
   implicit def noShrink[T]: Shrink[T] = Shrink.shrinkAny
 
-  "totalCents" must {
+  "simpleTotalCents" must {
     "add unit prices per item on the list, regardless of order" in {
       forAll(apples, oranges) { (as, os) =>
         val ps = shuffle(as ++ os)
-        sc.totalCents(ps) shouldBe as.length * 60 + os.length * 25
+        sc.simpleTotalCents(ps) shouldBe priceOfApples(as) + priceOfOranges(os)
+      }
+    }
+  }
+
+  "promoTotalCents" must {
+    "apply discounts to the total value" in {
+      forAll(apples, oranges) { (as, os) =>
+        val ps = shuffle(as ++ os)
+        sc.promoTotalCents(ps) shouldBe (priceOfApples(as) + priceOfOranges(os)
+                                         - sc.discountNforM(as, apple, 2, 1)
+                                         - sc.discountNforM(os, orange, 3, 2))
+      }
+    }
+  }
+
+  "discountNforM" must {
+    "return half of the prices sum total as discount for even length apples" in {
+      val discountApples = (listOf(delay(apple)), "discounted")
+      forAll(discountApples) { as =>
+        sc.discountNforM(as ++ as, apple, 2, 1) shouldBe priceOfApples(as)
+      }
+    }
+
+    "not count the extra apple as discount for odd length apples" in {
+      val discountApples = (listOf(delay(apple)), "discounted")
+      forAll(discountApples) { as =>
+        sc.discountNforM(as ++ (as :+ apple), apple, 2, 1) shouldBe priceOfApples(as)
+      }
+    }
+
+    "not apply discounts to lists without the discounted items" in {
+      forAll(oranges) { os =>
+        sc.discountNforM(os, apple, 2, 1) shouldBe 0
+      }
+    }
+
+    "offer 1 orange whenever 3 are bought" in {
+      val discountOranges = (listOf(delay(orange)), "discounted")
+      forAll(discountOranges) { os =>
+        sc.discountNforM(os ++ os ++ os, orange, 3, 2) shouldBe priceOfOranges(os)
+      }
+    }
+
+    "ignore the extra orange(s) whenever odd numbers are bought" in {
+      val discountOranges = (listOf(delay(orange)), "discounted")
+      forAll(discountOranges) { os =>
+        val oranges = os ++ os ++ os
+        sc.discountNforM(oranges :+ orange, orange, 3, 2) shouldBe priceOfOranges(os)
+        sc.discountNforM(oranges :+ orange :+ orange, orange, 3, 2) shouldBe priceOfOranges(os)
       }
     }
   }
@@ -24,4 +73,7 @@ class ShoppingCartSpec extends BaseSpec {
 
   val apples = listOf(delay(apple))
   val oranges = listOf(delay(orange))
+
+  def priceOfApples(ps: List[String]) = ps.length * 60
+  def priceOfOranges(ps: List[String]) = ps.length * 25
 }
