34441d77f3cbf4f8d5bacbf966aa9a681fee26c2
Hugo Ferreira <hugo@ferreira.cc>
2016-07-30 01:40:54 +0100

    Add simple cart total

--- src/main/scala/shoppingcheck/ShoppingCart.scala
+++ src/main/scala/shoppingcheck/ShoppingCart.scala
@@ -1,5 +1,8 @@
 package shoppingcheck
 
 class ShoppingCart {
+  val prices = Map("apple" -> 60, "orange" -> 25)
+
+  def totalCents(ps: List[String]): Int = ps.map(prices.getOrElse(_, 0)).sum
 
 }
--- src/test/scala/shoppingcheck/ShoppingCartSpec.scala
+++ src/test/scala/shoppingcheck/ShoppingCartSpec.scala
@@ -1,5 +1,27 @@
 package shoppingcheck
 
-class ShoppingCartSpec extends BaseSpec {
+import org.scalacheck.Gen._
+import org.scalacheck.Shrink
 
+import scala.util.Random._
+
+class ShoppingCartSpec extends BaseSpec {
+  implicit def noShrink[T]: Shrink[T] = Shrink.shrinkAny
+
+  "totalCents" must {
+    "add unit prices per item on the list, regardless of order" in {
+      forAll(apples, oranges) { (as, os) =>
+        val ps = shuffle(as ++ os)
+        sc.totalCents(ps) shouldBe as.length * 60 + os.length * 25
+      }
+    }
+  }
+
+  // Test Setup
+  val sc = new ShoppingCart
+  val apple = "apple"
+  val orange = "orange"
+
+  val apples = listOf(delay(apple))
+  val oranges = listOf(delay(orange))
 }
