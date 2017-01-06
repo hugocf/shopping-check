cdc2435bfe18caf1c6f57e5f5e5b3a615a3bc168
Hugo Ferreira <hugo@ferreira.cc>
2017-01-06 16:25:23 +0000

    Use the more appropriate `const` generator instead of `delay`

--- src/test/scala/shoppingcheck/ShoppingCartSpec.scala
+++ src/test/scala/shoppingcheck/ShoppingCartSpec.scala
@@ -54,14 +54,14 @@ class ShoppingCartSpec extends BaseSpec {
     "discount considers only a single type of product" must {
 
       "return half of the prices sum total as discount for even length apples" in {
-        val discountedApples = (listOf(delay(apple)), "discounted")
+        val discountedApples = (listOf(const(apple)), "discounted")
         forAll(discountedApples) { as =>
           sc.discountNforM(apple, 2, 1)(as ++ as) shouldBe costOfApples(as)
         }
       }
 
       "not count the extra apple as discount for odd length apples" in {
-        val discountedApples = (listOf(delay(apple)), "discounted")
+        val discountedApples = (listOf(const(apple)), "discounted")
         forAll(discountedApples) { as =>
           sc.discountNforM(apple, 2, 1)(as ++ (as :+ apple)) shouldBe costOfApples(as)
         }
@@ -74,14 +74,14 @@ class ShoppingCartSpec extends BaseSpec {
       }
 
       "offer 1 orange whenever 3 are bought" in {
-        val discountedOranges = (listOf(delay(orange)), "discounted")
+        val discountedOranges = (listOf(const(orange)), "discounted")
         forAll(discountedOranges) { os =>
           sc.discountNforM(orange, 3, 2)(os ++ os ++ os) shouldBe costOfOranges(os)
         }
       }
 
       "ignore the extra orange(s) whenever odd numbers are bought" in {
-        val discountedOranges = (listOf(delay(orange)), "discounted")
+        val discountedOranges = (listOf(const(orange)), "discounted")
         forAll(discountedOranges) { os =>
           val oranges = os ++ os ++ os
           sc.discountNforM(orange, 3, 2)(oranges :+ orange) shouldBe costOfOranges(os)
@@ -95,8 +95,8 @@ class ShoppingCartSpec extends BaseSpec {
       "offer all the cheapest products first if they are both of the same number" in {
         val sameLengthLists = for {
           n <- posNum[Int]
-          as <- listOfN(n, delay(apple))
-          bs <- listOfN(n, delay(banana))
+          as <- listOfN(n, const(apple))
+          bs <- listOfN(n, const(banana))
         } yield (as, bs)
 
         forAll(sameLengthLists) { case (as, bs) =>
@@ -108,7 +108,7 @@ class ShoppingCartSpec extends BaseSpec {
         val lessCheaperProducts = for {
           as <- apples
           len <- choose(0, as.length)
-          bs <- listOfN(len, delay(banana))
+          bs <- listOfN(len, const(banana))
         } yield (as, bs)
 
         forAll(lessCheaperProducts) { case (as, bs) =>
@@ -121,7 +121,7 @@ class ShoppingCartSpec extends BaseSpec {
         val lessCheaperProducts = for {
           as <- apples
           len <- posNum[Int] suchThat (_ > as.length)
-          bs <- listOfN(len, delay(banana))
+          bs <- listOfN(len, const(banana))
         } yield (as, bs)
 
         forAll(lessCheaperProducts) { case (as, bs) =>
@@ -140,10 +140,10 @@ class ShoppingCartSpec extends BaseSpec {
   val (banana, bananaPrice) = ("banana", 20)
   val (melon, melonPrice) = ("melon", 100)
 
-  val apples = listOf(delay(apple))
-  val oranges = listOf(delay(orange))
-  val bananas = listOf(delay(banana))
-  val melons = listOf(delay(melon))
+  val apples = listOf(const(apple))
+  val oranges = listOf(const(orange))
+  val bananas = listOf(const(banana))
+  val melons = listOf(const(melon))
 
   def costOfX(x: String, price: Int, ps: List[String]) = {
     require(ps.forall(_ == x), s"costOf${x.toLowerCase.capitalize} must contain only ${x}s")
